29631b1e071e0d00e79002900c7fbf6d2492230c
BarredEwe <barredEwe@gmail.com>
2025-02-16 00:43:55 +0300

    Update Prefire.rb

--- Formula/Prefire.rb
+++ Formula/Prefire.rb
@@ -1,16 +1,20 @@
 class Prefire < Formula
-	desc "CLI for generating tests with Prefire"
+	desc "A library for easily generating automatic Playbook (Demo) view, Tests using Preview"
 	homepage "https://github.com/barredewe/prefire"
-	url "https://github.com/barredewe/prefire/releases/download/1.0.0/prefire.tar.gz" # Укажите актуальный URL
-	sha256 "a1b2c3d4e5f6..." # Замените на реальный SHA-256 вашего .tar.gz
+	url "https://github.com/barredewe/prefire/releases/download/3.6.0/prefire.tar.gz"
+	sha256 "f6450dbbd5bb279b3681831eb9021e14470b6133efdccb2565d4e8bdf302b2a8"
+	license "Apache-2.0"
   
 	depends_on "sourcery" => :required
   
 	def install
-	  bin.install "prefire"
+		bin.install "prefire"
+
+		libexec.install "PreviewTests.stencil"
+		libexec.install "PreviewModels.stencil"
 	end
   
 	test do
-	  system "#{bin}/prefire", "--version"
+	  	system "#{bin}/prefire", "--version"
 	end
   end
