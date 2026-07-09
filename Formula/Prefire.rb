a94bbffe8214900cd3ac3b004c693a9a5ffe7e22
BarredEwe <barredEwe@gmail.com>
2026-07-09 12:07:59 +0300

    Prefire 5.7.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/Prefire.rb
+++ Formula/Prefire.rb
@@ -1,8 +1,8 @@
 class Prefire < Formula
 	desc "A library for easily generating automatic Playbook (Demo) view, Tests using Preview"
 	homepage "https://github.com/barredewe/prefire"
-	url "https://github.com/barredewe/prefire/releases/download/5.6.1/prefire.tar.gz"
-	sha256 "76fb96bbbc6d3b701e26c7a852f4252b6691038f7718afb86cee3e06d923b428"
+	url "https://github.com/barredewe/prefire/releases/download/5.7.0/prefire.tar.gz"
+	sha256 "8f8a1d95cadc705fdbd950a72aee95e5a59f90a41dcd17e300f95c8becc6121b"
 	license "Apache-2.0"
   
 	def install
